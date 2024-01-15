30dd15cd834515c76c9df56ac7f14cb2f263186d
Yarden Dadon <yarden.dadon@post.runi.ac.il>
2024-01-15 23:45:02 +0200

    testing inputs

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,11 +1,21 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        test
+        int[] input1 = {4, 2, 0, 1, 3, 6};
+
+
+        System.out.println(findMissingInt(input1));
+
+
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+    int missingNumber = 0;
+    for (int i = 0; i < array.length; i++){
+        for (int j = 0; j < array.length; j++){
+            if (array[j] != i){missingNumber = i;}
+        }   
+    }
+    return missingNumber;
     }
 
     public static int secondMaxValue(int [] array) {
