482b97b5572c8f25da518c0d3857ca41bdca0f44
Yarden Dadon <yarden.dadon@post.runi.ac.il>
2024-01-15 23:47:27 +0200

    again

--- ArrayOps.java
+++ ArrayOps.java
@@ -9,6 +9,7 @@ public class ArrayOps {
     }
     
     public static int findMissingInt (int [] array) {
+    if (array.length == 1){return 1;}
     int missingNumber = 0;
     for (int i = 0; i < array.length; i++){
         for (int j = 0; j < array.length; j++){
