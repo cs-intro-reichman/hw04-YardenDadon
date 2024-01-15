078b8708e0b8d03903c768152391a3f1c0bdab0e
Yarden Dadon <yarden.dadon@post.runi.ac.il>
2024-01-16 00:55:05 +0200

    Array1

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,22 +1,25 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        int[] input1 = {4, 2, 0, 1, 3, 6};
-
-
+        int[] input1 = {1, 0, 3};
         System.out.println(findMissingInt(input1));
-
-
     }
-    
     public static int findMissingInt (int [] array) {
-    if (array.length == 1){return 1;}
-    int missingNumber = 0;
-    for (int i = 0; i < array.length; i++){
-        for (int j = 0; j < array.length; j++){
-            if (array[j] != i){missingNumber = i;}
-        }   
-    }
-    return missingNumber;
+        if (array.length == 1) {return 1;}
+        int missingNumber = 0;
+        for (int comparison = 0; comparison < array.length; comparison++) {
+            boolean found = false;
+            for (int indexValue = 0; indexValue < array.length; indexValue++) {
+                if (array[indexValue] == comparison) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                missingNumber = comparison;
+                break; 
+            }
+        }
+        return missingNumber;
     }
 
     public static int secondMaxValue(int [] array) {
